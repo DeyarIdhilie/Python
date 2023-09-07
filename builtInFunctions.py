440d079d92bbcdba41a77a53b9dbaa190561f85a
DeyarIdhilie <deyaridhilie2001@gmail.com>
2023-09-07 21:28:32 +0300

    parse from int to float , float to int, str to int, str to float

--- builtInFunctions.py
+++ builtInFunctions.py
@@ -15,3 +15,14 @@ print(abs(-1.0))#1.0
 print(abs(1.0))#1.0
 print(abs(2.2))#2.2
 """print(abs('1'))#TypeError: bad operand type for abs(): 'str'"""
+
+#parsing
+print(int(1.2))#1
+print(float(1))#1.0
+i= 5
+print(float(i))#5.0
+print(int(3))#3
+print(int('342'))
+print(float('25.3'))
+"""print(float('25.3'))#ValueError: invalid literal for int() with base 10: '25.3'"""
+print(float('2'))#2.0
