fedb322756f10ac456c1dca25205e121654281a3
DeyarIdhilie <deyaridhilie2001@gmail.com>
2023-09-07 20:53:46 +0300

    some examples that show that python is strongly typed

---
+++ strongTyping.py
@@ -0,0 +1,11 @@
+#why is python a strongly typed language
+'''number= 2
+line= "world"+ number
+print(line)#TypeError: can only concatenate str (not "int") to str'''
+
+"""sentence = sentence + 2
+print(sentence) #RuntimeError: TypeError: can only concatenate str (not "int") to str"""
+
+'''sentence = sentence - '!'
+print(sentence) #runtimeerror TypeError: unsupported operand type(s) for -: 'str' and 'str'
+'''
