4d1352d20b41da95b3d3f8c1fe875f91bf9c6e05
DeyarIdhilie <deyaridhilie2001@gmail.com>
2023-09-07 21:25:45 +0300

    find the abs of a number

--- builtInFunctions.py
+++ builtInFunctions.py
@@ -6,3 +6,12 @@ print(min(1.1,-2,-44.1,0.0,4.3))#-44.1
 #max
 print(max(1,-1,0,10,-33))#10
 print(max(1.1,-2,-44.1,0.0,4.3))#4.3
+
+#abs
+print(abs(3))#3
+print(abs(0))#0
+print(abs(-3))#3
+print(abs(-1.0))#1.0
+print(abs(1.0))#1.0
+print(abs(2.2))#2.2
+"""print(abs('1'))#TypeError: bad operand type for abs(): 'str'"""
