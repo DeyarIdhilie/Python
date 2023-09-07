2b4eb51eca1ddb17dbbe34898d35acbdc5ae1020
DeyarIdhilie <deyaridhilie2001@gmail.com>
2023-09-07 17:03:02 +0300

    learn how to comment

---
+++ howToComment.py
@@ -0,0 +1,8 @@
+#this is how comments are made in python
+#it's a sinlge line comment
+'''using three single quote you could create
+a multiline comment
+'''
+"""or using three double quote 
+it still make a multiline comment
+"""
