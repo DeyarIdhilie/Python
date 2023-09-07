d7a7f02f7e5d6765f95f189365fc839cccf2ddff
DeyarIdhilie <deyaridhilie2001@gmail.com>
2023-09-07 21:16:54 +0300

    exponentiation

---
+++ power.py
@@ -0,0 +1,10 @@
+# a**b is a power of b
+# exponentiation
+print(3**2)#9
+print(1.1**2)#1.2100000000000002
+print(7.0**5)#16807.0
+print(4**.5)#2.0#the root of 4 is 2
+print(4**1.4)#6.964404506368992
+print(4**-2)#1/(4^2)=1/16=0.0625
+print(4**1.5)#8.0
+#note if any of the operands if float the output will be stord in float
