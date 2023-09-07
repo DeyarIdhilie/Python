945580f4dc4a422e0f1e4ce1523e7f339b2c504b
DeyarIdhilie <deyaridhilie2001@gmail.com>
2023-09-07 21:09:17 +0300

    learn about multiplication in python

---
+++ multiplication.py
@@ -0,0 +1,8 @@
+#the operator * is used for multiplication
+#if there is any float number in the operand the result will be float
+#the result is the product of a*b
+print(4*5) #20
+print(4* 7.5) #30.0
+#external note :4.8 can't be exactly represented in binary because it's a repeating fraction
+print(6*4.8) #28.799999999999997
+print(6.5 * 4.1)#26.65
