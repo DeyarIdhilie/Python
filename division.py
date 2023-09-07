da6d2e47776c44b2c72f45bd8602653c5f2c82fe
DeyarIdhilie <deyaridhilie2001@gmail.com>
2023-09-07 21:02:36 +0300

    division, floor division, mod

---
+++ division.py
@@ -0,0 +1,23 @@
+n1 = 5
+n2 = 2
+n3= 1.2
+n4 = 6
+n5= 7.5
+# the operator / perform true division
+# the result is stored as float
+print(n1/n2) #2.5
+# the operator // perform floor division which basiclly remove the fractional part of the quotient(round down)
+#if the operands are both interger the result is integer otherwise if any or both are float the result is float
+print(n1//n2) #2
+print(n1/n3) #4.166666666666667
+print(n1//n3) #4.0
+print(n4/n2) #3.0
+print(n4//n2) #3
+print(n5/n1) #1.5
+print(n5//n1)#1.0
+print(n5/n3) #6.25
+print(n5//n3)#6.0
+print(n5/n4) #1.25
+# to find the reminder after the division
+print(n1%n2) #1
+print(n1%n3) #0.20000000000000018
